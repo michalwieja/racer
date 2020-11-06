33e3942e9a1705c09c70edf6d1bfdccec1d13566
mwConceptX <mwieja@yahoo.com>
2020-11-06 18:56:53 +0100

    first comit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,136 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="wrapper">
+    <h1>racer</h1>
+    <div class="game">
+      <div class="left"></div>
+      <div class="right"></div>
+      <div class="car"></div>
+      <div class="rival"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  mounted() {
+    const car = document.querySelector(".car");
+    const rival = document.querySelector(".rival");
+
+    function moveLeft() {
+      let left = car.offsetLeft;
+      left -= 100;
+      if (left < 0) {
+        left = 0;
+      }
+      car.style.left = left + "px";
+    }
+    function moveRight() {
+      let left = car.offsetLeft;
+      left += 100;
+      if (left > 200) {
+        left = 200;
+      }
+      car.style.left = left + "px";
+    }
+
+    document.addEventListener("keydown", e => {
+      if (e.key === "ArrowLeft") {
+        moveLeft();
+      } else if (e.key === "ArrowRight") {
+        moveRight();
+      }
+    });
+
+    const left = document.querySelector(".left");
+    const right = document.querySelector(".right");
+    left.addEventListener("touchstart", moveLeft);
+    right.addEventListener("touchstart", moveRight);
+    rival.addEventListener("animationiteration", () => {
+      let random = Math.floor(Math.random() * 3) * 100;
+
+      rival.style.left = random + "px";
+    });
+    setInterval(() => {
+      let carLeft = car.offsetLeft;
+      let rivalLeft = rival.offsetLeft;
+      let rivalTop = rival.offsetTop;
+
+      if (carLeft === rivalLeft && rivalTop > 300) {
+        alert("ok");
+      }
+    }, 10);
   }
 };
 </script>
+
+
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.wrapper {
+  height: 100vh;
+  background: #333;
+  color: white;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    margin: 20px;
+  }
+}
+.game {
+  height: 600px;
+  width: 300px;
+  border: 1px solid white;
+  position: relative;
+  overflow: hidden;
+}
+.left,
+.right {
+  width: 50%;
+  position: absolute;
+  height: 100%;
+}
+.right {
+  left: 50%;
+}
+.car {
+  position: absolute;
+  width: 100px;
+  height: 150px;
+  background-color: #fff;
+  bottom: 0;
+  left: 100px;
+}
+
+.rival {
+  position: absolute;
+  width: 100px;
+  height: 150px;
+  background-color: #fab;
+  top: 0;
+  left: 0px;
+  animation: rivalMove 1s infinite linear;
+}
+
+@keyframes rivalMove {
+  from {
+    top: 0;
+  }
+  to {
+    top: 600px;
+  }
 }
 </style>
+
+
